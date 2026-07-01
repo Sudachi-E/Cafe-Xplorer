f91a2c2c5dcee44e9091a8536fb919c2c96359b1
Sudachi-E <124676538+Sudachi-E@users.noreply.github.com>
2026-07-01 21:50:04 +0100

    Added navigation support for classic controller and nunchuck / support for other controllers using the Wii U keyboard

--- source/input/WPADInput.h
+++ source/input/WPADInput.h
@@ -24,14 +24,23 @@ public:
             data.rightStickX = kpad.pro.rightStick.x;
             data.rightStickY = kpad.pro.rightStick.y;
         } else if (kpad.extensionType == WPAD_EXT_CLASSIC || kpad.extensionType == WPAD_EXT_MPLUS_CLASSIC) {
-            data.buttons_h = kpad.hold;
-            data.buttons_d = kpad.trigger;
-            data.buttons_r = kpad.release;
+            data.buttons_h = MapWiiButtons(kpad.hold) | MapClassicButtons(kpad.classic.hold);
+            data.buttons_d = MapWiiButtons(kpad.trigger) | MapClassicButtons(kpad.classic.trigger);
+            data.buttons_r = MapWiiButtons(kpad.release) | MapClassicButtons(kpad.classic.release);
             data.leftStickX  = kpad.classic.leftStick.x;
             data.leftStickY  = kpad.classic.leftStick.y;
             data.rightStickX = kpad.classic.rightStick.x;
             data.rightStickY = kpad.classic.rightStick.y;
-        } else if (kpad.extensionType == WPAD_EXT_CORE) {
+        } else if (kpad.extensionType == WPAD_EXT_NUNCHUK || kpad.extensionType == WPAD_EXT_MPLUS_NUNCHUK) {
+            data.buttons_h = MapWiiButtons(kpad.hold) | MapNunchukButtons(kpad.nunchuk.hold)
+                           | NunchukStickToInputButtons(kpad.nunchuk.stick);
+            data.buttons_d = MapWiiButtons(kpad.trigger) | MapNunchukButtons(kpad.nunchuk.trigger);
+            data.buttons_r = MapWiiButtons(kpad.release) | MapNunchukButtons(kpad.nunchuk.release);
+            data.leftStickX  = kpad.nunchuk.stick.x;
+            data.leftStickY  = kpad.nunchuk.stick.y;
+            data.rightStickX = 0.0f;
+            data.rightStickY = 0.0f;
+        } else if (kpad.extensionType == WPAD_EXT_CORE || kpad.extensionType == WPAD_EXT_MPLUS) {
             data.buttons_h = MapWiiButtons(kpad.hold);
             data.buttons_d = MapWiiButtons(kpad.trigger);
             data.buttons_r = MapWiiButtons(kpad.release);
@@ -88,6 +97,42 @@ private:
         if (p & WPAD_PRO_STICK_L_EMULATION_DOWN)  m |= Input::BUTTON_DOWN;
         if (p & WPAD_PRO_STICK_L_EMULATION_LEFT)  m |= Input::BUTTON_LEFT;
         if (p & WPAD_PRO_STICK_L_EMULATION_RIGHT) m |= Input::BUTTON_RIGHT;
+        return m;
+    }
+
+    static uint32_t MapClassicButtons(uint32_t p) {
+        uint32_t m = 0;
+        if (p & WPAD_CLASSIC_BUTTON_A)      m |= Input::BUTTON_A;
+        if (p & WPAD_CLASSIC_BUTTON_B)      m |= Input::BUTTON_B;
+        if (p & WPAD_CLASSIC_BUTTON_X)      m |= Input::BUTTON_X;
+        if (p & WPAD_CLASSIC_BUTTON_Y)      m |= Input::BUTTON_Y;
+        if (p & WPAD_CLASSIC_BUTTON_LEFT)   m |= Input::BUTTON_LEFT;
+        if (p & WPAD_CLASSIC_BUTTON_RIGHT)  m |= Input::BUTTON_RIGHT;
+        if (p & WPAD_CLASSIC_BUTTON_UP)     m |= Input::BUTTON_UP;
+        if (p & WPAD_CLASSIC_BUTTON_DOWN)   m |= Input::BUTTON_DOWN;
+        if (p & WPAD_CLASSIC_BUTTON_PLUS)   m |= Input::BUTTON_PLUS;
+        if (p & WPAD_CLASSIC_BUTTON_MINUS)  m |= Input::BUTTON_MINUS;
+        if (p & WPAD_CLASSIC_BUTTON_HOME)   m |= Input::BUTTON_HOME;
+        if (p & WPAD_CLASSIC_BUTTON_L)      m |= Input::BUTTON_L;
+        if (p & WPAD_CLASSIC_BUTTON_R)      m |= Input::BUTTON_R;
+        if (p & WPAD_CLASSIC_BUTTON_ZL)     m |= Input::BUTTON_ZL;
+        if (p & WPAD_CLASSIC_BUTTON_ZR)     m |= Input::BUTTON_ZR;
+        return m;
+    }
+
+    static uint32_t MapNunchukButtons(uint32_t p) {
+        uint32_t m = 0;
+        if (p & WPAD_NUNCHUK_BUTTON_C)      m |= Input::BUTTON_ZR;
+        if (p & WPAD_NUNCHUK_BUTTON_Z)      m |= Input::BUTTON_ZL;
+        return m;
+    }
+
+    static uint32_t NunchukStickToInputButtons(const KPADVec2D &stick) {
+        uint32_t m = 0;
+        if (stick.x > 0.25f)  m |= Input::BUTTON_RIGHT;
+        if (stick.x < -0.25f) m |= Input::BUTTON_LEFT;
+        if (stick.y > 0.25f)  m |= Input::BUTTON_UP;
+        if (stick.y < -0.25f) m |= Input::BUTTON_DOWN;
         return m;
     }
 };
--- source/utils/Keyboard.cpp
+++ source/utils/Keyboard.cpp
@@ -12,6 +12,82 @@ std::string     Keyboard::sInputBuffer;
 bool            Keyboard::sPendingConfirmed = false;
 std::function<void(bool, const std::string&)> Keyboard::sCallback;
 
+static uint32_t MapWiimoteToVPADButtons(uint32_t wiimoteButtons) {
+    uint32_t vpadButtons = 0;
+    if (wiimoteButtons & WPAD_BUTTON_LEFT)   vpadButtons |= VPAD_BUTTON_LEFT;
+    if (wiimoteButtons & WPAD_BUTTON_RIGHT)  vpadButtons |= VPAD_BUTTON_RIGHT;
+    if (wiimoteButtons & WPAD_BUTTON_DOWN)   vpadButtons |= VPAD_BUTTON_DOWN;
+    if (wiimoteButtons & WPAD_BUTTON_UP)     vpadButtons |= VPAD_BUTTON_UP;
+    if (wiimoteButtons & WPAD_BUTTON_A)      vpadButtons |= VPAD_BUTTON_A;
+    if (wiimoteButtons & WPAD_BUTTON_B)      vpadButtons |= VPAD_BUTTON_B;
+    if (wiimoteButtons & WPAD_BUTTON_1)      vpadButtons |= VPAD_BUTTON_X;
+    if (wiimoteButtons & WPAD_BUTTON_2)      vpadButtons |= VPAD_BUTTON_Y;
+    if (wiimoteButtons & WPAD_BUTTON_PLUS)   vpadButtons |= VPAD_BUTTON_PLUS;
+    if (wiimoteButtons & WPAD_BUTTON_MINUS)  vpadButtons |= VPAD_BUTTON_MINUS;
+    if (wiimoteButtons & WPAD_BUTTON_HOME)   vpadButtons |= VPAD_BUTTON_HOME;
+    return vpadButtons;
+}
+
+static uint32_t MapClassicToVPADButtons(uint32_t classicButtons) {
+    uint32_t vpadButtons = 0;
+    if (classicButtons & WPAD_CLASSIC_BUTTON_A)      vpadButtons |= VPAD_BUTTON_A;
+    if (classicButtons & WPAD_CLASSIC_BUTTON_B)      vpadButtons |= VPAD_BUTTON_B;
+    if (classicButtons & WPAD_CLASSIC_BUTTON_X)      vpadButtons |= VPAD_BUTTON_X;
+    if (classicButtons & WPAD_CLASSIC_BUTTON_Y)      vpadButtons |= VPAD_BUTTON_Y;
+    if (classicButtons & WPAD_CLASSIC_BUTTON_LEFT)   vpadButtons |= VPAD_BUTTON_LEFT;
+    if (classicButtons & WPAD_CLASSIC_BUTTON_RIGHT)  vpadButtons |= VPAD_BUTTON_RIGHT;
+    if (classicButtons & WPAD_CLASSIC_BUTTON_UP)     vpadButtons |= VPAD_BUTTON_UP;
+    if (classicButtons & WPAD_CLASSIC_BUTTON_DOWN)   vpadButtons |= VPAD_BUTTON_DOWN;
+    if (classicButtons & WPAD_CLASSIC_BUTTON_PLUS)   vpadButtons |= VPAD_BUTTON_PLUS;
+    if (classicButtons & WPAD_CLASSIC_BUTTON_MINUS)  vpadButtons |= VPAD_BUTTON_MINUS;
+    if (classicButtons & WPAD_CLASSIC_BUTTON_HOME)   vpadButtons |= VPAD_BUTTON_HOME;
+    if (classicButtons & WPAD_CLASSIC_BUTTON_L)      vpadButtons |= VPAD_BUTTON_L;
+    if (classicButtons & WPAD_CLASSIC_BUTTON_R)      vpadButtons |= VPAD_BUTTON_R;
+    if (classicButtons & WPAD_CLASSIC_BUTTON_ZL)     vpadButtons |= VPAD_BUTTON_ZL;
+    if (classicButtons & WPAD_CLASSIC_BUTTON_ZR)     vpadButtons |= VPAD_BUTTON_ZR;
+    return vpadButtons;
+}
+
+static uint32_t MapNunchukToVPADButtons(uint32_t nunchukButtons) {
+    uint32_t vpadButtons = 0;
+    if (nunchukButtons & WPAD_NUNCHUK_BUTTON_C) vpadButtons |= VPAD_BUTTON_ZR;
+    if (nunchukButtons & WPAD_NUNCHUK_BUTTON_Z) vpadButtons |= VPAD_BUTTON_ZL;
+    return vpadButtons;
+}
+
+static uint32_t NunchukStickToVPADButtons(const KPADVec2D &stick) {
+    uint32_t vpadButtons = 0;
+    if (stick.x > 0.25f)  vpadButtons |= VPAD_BUTTON_RIGHT;
+    if (stick.x < -0.25f) vpadButtons |= VPAD_BUTTON_LEFT;
+    if (stick.y > 0.25f)  vpadButtons |= VPAD_BUTTON_UP;
+    if (stick.y < -0.25f) vpadButtons |= VPAD_BUTTON_DOWN;
+    return vpadButtons;
+}
+
+static uint32_t MapProToVPADButtons(uint32_t proButtons) {
+    uint32_t vpadButtons = 0;
+    if (proButtons & WPAD_PRO_BUTTON_A)      vpadButtons |= VPAD_BUTTON_A;
+    if (proButtons & WPAD_PRO_BUTTON_B)      vpadButtons |= VPAD_BUTTON_B;
+    if (proButtons & WPAD_PRO_BUTTON_X)      vpadButtons |= VPAD_BUTTON_X;
+    if (proButtons & WPAD_PRO_BUTTON_Y)      vpadButtons |= VPAD_BUTTON_Y;
+    if (proButtons & WPAD_PRO_BUTTON_LEFT)   vpadButtons |= VPAD_BUTTON_LEFT;
+    if (proButtons & WPAD_PRO_BUTTON_RIGHT)  vpadButtons |= VPAD_BUTTON_RIGHT;
+    if (proButtons & WPAD_PRO_BUTTON_UP)     vpadButtons |= VPAD_BUTTON_UP;
+    if (proButtons & WPAD_PRO_BUTTON_DOWN)   vpadButtons |= VPAD_BUTTON_DOWN;
+    if (proButtons & WPAD_PRO_BUTTON_PLUS)   vpadButtons |= VPAD_BUTTON_PLUS;
+    if (proButtons & WPAD_PRO_BUTTON_MINUS)  vpadButtons |= VPAD_BUTTON_MINUS;
+    if (proButtons & WPAD_PRO_BUTTON_HOME)   vpadButtons |= VPAD_BUTTON_HOME;
+    if (proButtons & WPAD_PRO_BUTTON_L)      vpadButtons |= VPAD_BUTTON_L;
+    if (proButtons & WPAD_PRO_BUTTON_R)      vpadButtons |= VPAD_BUTTON_R;
+    if (proButtons & WPAD_PRO_TRIGGER_ZL)    vpadButtons |= VPAD_BUTTON_ZL;
+    if (proButtons & WPAD_PRO_TRIGGER_ZR)    vpadButtons |= VPAD_BUTTON_ZR;
+    if (proButtons & WPAD_PRO_STICK_L_EMULATION_LEFT)  vpadButtons |= VPAD_BUTTON_LEFT;
+    if (proButtons & WPAD_PRO_STICK_L_EMULATION_RIGHT) vpadButtons |= VPAD_BUTTON_RIGHT;
+    if (proButtons & WPAD_PRO_STICK_L_EMULATION_UP)    vpadButtons |= VPAD_BUTTON_UP;
+    if (proButtons & WPAD_PRO_STICK_L_EMULATION_DOWN)  vpadButtons |= VPAD_BUTTON_DOWN;
+    return vpadButtons;
+}
+
 static int SDLCALL KeyboardEventWatch(void* /*userdata*/, SDL_Event* event) {
     if (Keyboard::sState == Keyboard::State::Idle) {
         return 1;
@@ -98,6 +174,8 @@ void Keyboard::Update() {
 
     VPADStatus    vpadStatus{};
     VPADReadError vpadError;
+    bool vpadValid = false;
+
     if (VPADRead(VPAD_CHAN_0, &vpadStatus, 1, &vpadError) > 0 &&
         vpadError == VPAD_READ_SUCCESS) {
         VPADTouchData calibrated{};
@@ -105,14 +183,55 @@ void Keyboard::Update() {
         vpadStatus.tpNormal    = calibrated;
         vpadStatus.tpFiltered1 = calibrated;
         vpadStatus.tpFiltered2 = calibrated;
-        SDL_WiiUSetSWKBDVPAD(&vpadStatus);
+        vpadValid = true;
     }
 
-    KPADStatus kpadStatus{};
+    KPADStatus kpadStatus[4]{};
     for (int i = 0; i < 4; ++i) {
-        if (KPADReadEx(static_cast<KPADChan>(i), &kpadStatus, 1, nullptr) > 0) {
-            SDL_WiiUSetSWKBDKPAD(i, &kpadStatus);
+        KPADError kpadError;
+        if (KPADReadEx(static_cast<KPADChan>(i), &kpadStatus[i], 1, &kpadError) > 0 &&
+            kpadError == KPAD_ERROR_OK) {
+            switch (kpadStatus[i].extensionType) {
+            case WPAD_EXT_PRO_CONTROLLER:
+                vpadStatus.hold    |= MapProToVPADButtons(kpadStatus[i].pro.hold);
+                vpadStatus.trigger |= MapProToVPADButtons(kpadStatus[i].pro.trigger);
+                vpadStatus.release |= MapProToVPADButtons(kpadStatus[i].pro.release);
+                vpadValid = true;
+                break;
+            case WPAD_EXT_CLASSIC:
+            case WPAD_EXT_MPLUS_CLASSIC:
+                vpadStatus.hold    |= MapWiimoteToVPADButtons(kpadStatus[i].hold)
+                                    | MapClassicToVPADButtons(kpadStatus[i].classic.hold);
+                vpadStatus.trigger |= MapWiimoteToVPADButtons(kpadStatus[i].trigger)
+                                    | MapClassicToVPADButtons(kpadStatus[i].classic.trigger);
+                vpadStatus.release |= MapWiimoteToVPADButtons(kpadStatus[i].release)
+                                    | MapClassicToVPADButtons(kpadStatus[i].classic.release);
+                vpadValid = true;
+                break;
+            case WPAD_EXT_NUNCHUK:
+            case WPAD_EXT_MPLUS_NUNCHUK:
+                vpadStatus.hold    |= MapWiimoteToVPADButtons(kpadStatus[i].hold)
+                                    | MapNunchukToVPADButtons(kpadStatus[i].nunchuk.hold)
+                                    | NunchukStickToVPADButtons(kpadStatus[i].nunchuk.stick);
+                vpadStatus.trigger |= MapWiimoteToVPADButtons(kpadStatus[i].trigger)
+                                    | MapNunchukToVPADButtons(kpadStatus[i].nunchuk.trigger);
+                vpadStatus.release |= MapWiimoteToVPADButtons(kpadStatus[i].release)
+                                    | MapNunchukToVPADButtons(kpadStatus[i].nunchuk.release);
+                vpadValid = true;
+                break;
+            case WPAD_EXT_CORE:
+            case WPAD_EXT_MPLUS:
+                vpadStatus.hold    |= MapWiimoteToVPADButtons(kpadStatus[i].hold);
+                vpadStatus.trigger |= MapWiimoteToVPADButtons(kpadStatus[i].trigger);
+                vpadStatus.release |= MapWiimoteToVPADButtons(kpadStatus[i].release);
+                vpadValid = true;
+                break;
+            }
         }
+    }
+
+    if (vpadValid) {
+        SDL_WiiUSetSWKBDVPAD(&vpadStatus);
     }
 
     SDL_PumpEvents();
